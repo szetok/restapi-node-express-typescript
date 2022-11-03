70b2529cb44cf11264416003b34415e10a242ea2
Kevin Szeto <kszeto@bleacherreport.com>
2022-11-03 14:50:15 -0400

    implement get, update, and delete operations

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -12,3 +12,33 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.status(201).json({ message: "Created the todo.", createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoToUpdate = TODOS.find((todo) => todo.id === todoId);
+  if (todoToUpdate) {
+    todoToUpdate.text = updatedText;
+  } else {
+    throw new Error("Could not find todo!");
+  }
+
+  res.json({ message: "Updated!", updatedTodo: todoToUpdate });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const deleteIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (deleteIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+  TODOS.splice(deleteIndex, 1);
+
+  res.json({ message: "Deleted!" });
+};
